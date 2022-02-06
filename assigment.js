f36e792d1e250f477782e64ff21fed83ae379538
Sanch1rr <97089298+Sanch1rr@users.noreply.github.com>
2022-02-06 16:29:39 +0800

    Add files via upload

---
+++ assigment.js
@@ -0,0 +1,114 @@
+//loop assigment
+
+//1-r bodlogo
+// let number = parseInt(prompt("Please enter your number:"));
+
+
+
+
+
+// let total=0;
+// for(let i=1; i<= number; i++){
+// total= total+i;
+
+
+// }
+// document.getElementById("text").innerText = total;
+
+// let num1 = parseInt(prompt("Please enter your number:"));
+// let  = parseInt(prompt("Please enter your number:"));
+
+
+//2-r bodlogo
+
+// var a= prompt("Хэдэн тоо оруулахаа бичнэ үү")
+//         var b =a ;
+//         var c;
+//         var d =0;
+//         var i = 1;
+
+//         while(a > 0){
+//             c = parseInt(prompt( i + "дх Тоогоо оруулна уу : "))
+//             i++;
+//             d = c + d;
+//             a--
+//         }
+        
+//         document.getElementById("text").innerText = a = d / b;
+
+
+// 3-r bodlogo
+
+// let number = parseInt(prompt("Тоогоо оруулна уу:"));
+
+
+// let urjver = " "
+// for (let i=1; i <= 10; i++){
+//     urjver = urjver + "\n" + i + "x" + number + "=" +i * number;
+// }
+// alert(urjver);
+
+//4-r bodlogo
+
+
+// let number = parseInt(prompt("Тооны хязгаараа оруулна уу:"));
+// let number2 = parseInt(prompt("Хуваах тоогоо оруулна уу:"));
+
+
+// let divisible = 0
+// for (let i=1; i <=number; i++ )
+// if (i%number2===0){
+// divisible = divisible + i + ",";
+// }
+// alert(divisible);
+
+// 5-r bodlogo
+
+// let digits = (prompt("Please enter your digits:"));
+// let number = 0;
+// for (let i=1; i<=digits.length;i++){
+//     number = number + parseInt(digits.charAt(digits.length - i));
+// }
+// alert(number);
+
+
+//array assigment
+//bodlogo-1
+
+// let myAlphabet = ["A","B","C","D","E","F","G"];
+// function myAlphabetLength(){
+//     console.log(myAlphabet.length);
+//     if(myAlphabet.length<4){
+//         console.log("4oos baga")
+//     }else console.log("4oos ih")
+
+// }
+
+// console.log(myAlphabetLength());
+
+//bodlogo-2
+// let planets = ["Jupiter","Mars","Saturn", "Pluto", "Earth" ]
+// planets.forEach(function (item, index) {
+//     console.log(index,item)
+// })
+
+
+//bodlogo-3
+
+// let wowDatatypes =  ["String", 3, 3.2, "A", true];
+// for(i=0; i<wowDatatypes.length;i++){
+//     console.log(i+" - " + typeof wowDatatypes[i])
+// }
+
+
+// //bodlogo-4
+// let student1Courses = ['Математик', 'Англи хэл', 'Програмчлал'];
+// let student2Courses = ['Газар зүй', 'Испани', 'Програмчлал'];
+// for(i=0; i < student1Courses.length; i++){
+//     if(student2Courses.indexOf(student1Courses[i]) > -1){
+//         console.log(student1Courses[i]);
+
+//     }
+
+// }
+
